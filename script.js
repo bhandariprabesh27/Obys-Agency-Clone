1da8557825466b663d6ba5ffb596b761ae034409
Prabesh Bhandari <prabesh.chitwan@gmail.com>
2024-09-15 22:56:30 +0545

    loader intermediate commit

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.from(".line h1", {
+  y: 150,
+  stagger: 0.25,
+  duration: 0.6,
+  delay: 0.5,
+});
+
+var h5timer = document.querySelector("#line1-part1 h5");
+var grow = 0;
+setInterval(function () {
+  if (grow < 100) {
+    h5timer.innerHTML = grow++;
+  } else {
+    h5timer.innerHTML = grow;
+  }
+}, 33);
+
+var tl = gsap.timeline();
+tl.to("#loader", {
+  opacity: 0,
+  duration: 0.4,
+  delay: 4,
+});
